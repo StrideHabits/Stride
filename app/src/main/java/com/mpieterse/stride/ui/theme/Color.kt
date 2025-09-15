ca0ffeb95110ef796882740db41d96c80da917dc
musa <ST10293362@vcconnect.edu.za>
2025-09-15 20:19:23 +0200

    Skeleton basic screens added

--- app/src/main/java/com/mpieterse/stride/ui/theme/Color.kt
+++ app/src/main/java/com/mpieterse/stride/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.mpieterse.stride.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Core brand tokens (temporary until final palette):
+val Orange = Color(0xFFFF9800)
+val HeaderDark = Color(0xFF0F0F13)
+val SurfaceCard = Color(0xFFFFFFFF)
+val SurfaceSubtle = Color(0xFFF5F5F5)
+val Border = Color(0xFFE0E0E0)
+val TextMuted = Color(0xFF737373)
